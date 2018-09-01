bff3452fcd035925ee46b62e24bc5b460abd9b2c
Rafal Becla <rafal.becla@gmail.com>
2018-09-01 17:36:04 +0200

    initialized QuestionIterator class

---
+++ AoSExpert/src/main/java/com/codecool/app/QuestionIterator.java
@@ -0,0 +1,19 @@
+package com.codecool.app;
+
+import java.util.Iterator;
+import com.codecool.app.rules.*;
+
+public class QuestionIterator implements Iterator<Question>{
+
+    public QuestionIterator(){
+
+    }
+
+    public boolean hasNext() {
+        return false;
+    }
+
+    public Question next() {
+        return null;
+    }
+}
