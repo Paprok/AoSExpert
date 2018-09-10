8e452dce50d9323d0da05ddbb938b5fc95778103
Rafal Becla <rafal.becla@gmail.com>
2018-09-10 16:29:03 +0200

    changed main to create ESProvider object with needed arguments

--- AoSExpert/src/main/java/com/codecool/app/App.java
+++ AoSExpert/src/main/java/com/codecool/app/App.java
@@ -1,5 +1,8 @@
 package com.codecool.app;
 
+import com.codecool.app.parsers.FactParser;
+import com.codecool.app.parsers.RuleParser;
+
 /**
  * Hello world!
  *
@@ -8,6 +11,15 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        String factPath = "/home/ferned/Code/Java/SI_5/AoSExpert/src/main/java/com/codecool/app/dataBase/facts.xml";
+        factPath = "facts.xml";
+        String rulePath = "/home/ferned/Code/Java/SI_5/AoSExpert/src/main/java/com/codecool/app/dataBase/rules.xml";
+        rulePath = "rules.xml";
+        String ruleTag = "Rule";
+        String factTag = "Fact";
+        FactParser factParser = new FactParser(factPath, factTag);
+        RuleParser ruleParser = new RuleParser(rulePath, ruleTag);
+        ESProvider app = new ESProvider(factParser, ruleParser);
+        
     }
 }
